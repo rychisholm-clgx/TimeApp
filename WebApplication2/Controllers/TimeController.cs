95379adada08833db5c79208fcc1832d55ac8493
Rylee Chisholm <rychisholm@corelogic.com>
2022-06-16 13:29:46 -0500

    adding files

---
+++ WebApplication2/Controllers/TimeController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication2.Controllers
+{
+    [Route("api/time")]
+    [ApiController]
+    public class TimeController : ControllerBase
+    {
+        //gets the current time and date
+        [HttpGet]
+        public string ListTimeZone()
+        {
+            return DateTime.Now.ToString();
+        }
+    }
+}
